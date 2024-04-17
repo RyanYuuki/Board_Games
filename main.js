021205a26388e4d0dc31d9c916d55cdef00faa05
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-04-17 08:29:50 +0530

    FIX: different approach with draw handling

--- main.js
+++ main.js
@@ -4,19 +4,18 @@ let isRunning = true;
 const winningCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [2, 4, 6], [0, 4, 8] 
+    [2, 4, 6], [0, 4, 8]
 ];
 let draw = false;
 let userPoints = 0;
-let computerPoints = 0; 
+let computerPoints = 0;
 const pointsElmt = document.getElementById("Points");
 const resetElmt = document.getElementById("resetBtn");
 const remarkElmt = document.getElementById("Remark");
 const deciderElmt = document.getElementById("Decider");
 const msgElmt = document.getElementById("msg");
-
-function Game() { 
-    resetElmt.addEventListener("click" , () => {
+function Game() {
+    resetElmt.addEventListener("click", () => {
         resetGame();
     });
     for (let i = 0; i < boardBtns[0].children.length; i++) {
@@ -37,32 +36,29 @@ function Game() {
     }
 }
 
-function computerTurn()
-{
-let computerMove = Math.floor(Math.random() * 8);
-if (boardBtns[0].children[computerMove].textContent != '') {
-  computerMove = Math.floor(Math.random() * 8);
-  let index = 0;
-  for (let j = 0; j < 8; j++) {
-    if (boardBtns[0].children[j].textContent != '') {
-      index++;
+function computerTurn() {
+    let computerMove = Math.floor(Math.random() * 8);
+    if (boardBtns[0].children[computerMove].textContent != '') {
+        computerMove = Math.floor(Math.random() * 8);
+        let index = 0;
+        for (let j = 0; j < 8; j++) {
+            if (boardBtns[0].children[j].textContent != '') {
+                index++;
+            }
+        }
+        if (index == 8) {
+            isRunning = false;
+        } else {
+            computerTurn();
+        }
+    } else {
+        boardBtns[0].children[computerMove].textContent = 'O';
+        playerTurn = true;
+        checkWinner();
     }
-  }
-  if (index == 8) {
-    checkWinner();
-    alert("Tie Game");
-    isRunning = false;
-  } else {
-    computerTurn();
-  }
-} else {
-  boardBtns[0].children[computerMove].textContent = 'O';
-  playerTurn = true;
-  checkWinner();
-}
 }
 
-function checkWinner() {
+function checkWinner(index) {
     let isBoardFull = true;
 
     for (let combo of winningCombos) {
@@ -77,25 +73,28 @@ function checkWinner() {
                 remarkElmt.textContent = "Damn, You're Good!";
                 deciderElmt.textContent = "You Won!";
                 isRunning = false;
-            } else {
+                draw = false;
+            } else if (boardBtns[0].children[combo[2]].textContent === 'O') {
                 computerPoints++;
                 msgElmt.style.display = "grid";
                 remarkElmt.textContent = "You Suck at this!";
                 deciderElmt.textContent = "You Lost!";
                 isRunning = false;
+                draw = false;
             }
-            pointsElmt.textContent = `User Points --> ${userPoints} Computer Points --> ${computerPoints}`;
         }
+        else {
+            console.log(index);
+            if (index == 8) {
+                draw = true;
+            }
 
-        for (let i = 0; i < 3; i++) {
-            if (boardBtns[0].children[combo[i]].textContent === '') {
-                isBoardFull = false;
-                break;
-            }
         }
+        console.log(index);
+        pointsElmt.textContent = `User Points --> ${userPoints} Computer Points --> ${computerPoints}`;
     }
 
-    if (isBoardFull) {
+    if (draw) {
         msgElmt.style.display = "grid";
         deciderElmt.textContent = "Tied!";
         remarkElmt.textContent = "Try Harder";
@@ -103,7 +102,7 @@ function checkWinner() {
     }
 }
 
-function resetGame(){
+function resetGame() {
     for (let i = 0; i < boardBtns[0].children.length; i++) {
         boardBtns[0].children[i].textContent = '';
     }
